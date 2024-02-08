fe23a0e136af579f56b04af657d59ffaf4d96cf9
inacio4234 <121254172+inacio4234@users.noreply.github.com>
2024-02-08 15:50:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640, 480);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+    tintColor = "";
+}
+
+function draw(){
+    Image(video, 0, 0, 640, 480);
+    tint(tintColor);
+}
+
+function takeSnapshot(){
+    save('studentName.png');
+}
+
+function filterTint(){
+    tintColor = document.getElementById("colorName").value;
+}
